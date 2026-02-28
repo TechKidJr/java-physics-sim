545f285837555beb88a6cc5515dbb360e3ab5bfb
Advaith <advaithgirish12@gmail.com>
2026-02-28 14:40:24 -0500

    [#14] Added the window using Jframe and java swing

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import javax.swing.JFrame;
+import javax.swing.ImageIcon;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.*;
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) throws Exception {
+        // Test Print Line for Java
         System.out.println("TestPrint Line for Java");
+        JFrame frame = new JFrame("Physics Sim");
+        System.out.println("Looking for resource: " + Main.class.getResource("/bionic.png"));
+        javax.swing.JLabel label = new javax.swing.JLabel("Hello World!");
+
+
+        // Setting the size of the frame.
+        frame.setSize(1920, 1080);
+
+        // Setting the bPaneackground color of the frame to Black (for now).
+        frame.getContentPane().setBackground(java.awt.Color.BLACK);
+        frame.getContentPane().add(label);
+
+        Image icon = new ImageIcon(Main.class.getResource("/bionic.png")).getImage();
+
+        frame.setIconImage(icon);
+
+
+        // Setting the frame to visible :shock:
+        frame.setVisible(true);
+
+        // https://bionic.wowzatm.me/bionic.png
+
+        frame.getContentPane().setForeground(Color.WHITE);
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
