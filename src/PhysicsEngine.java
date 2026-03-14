dcfd2b02d4f34735570b6edc310a53873c839258
Advaith <advaithgirish12@gmail.com>
2026-03-14 19:39:55 -0400

    [#39] Did some constants, still got a lot to do.

--- src/PhysicsEngine.java
+++ src/PhysicsEngine.java
@@ -1,8 +1,19 @@
 import com.bulletphysics.*;
+
+import java.sql.Time;
+
 import org.jogamp.vecmath.Vector3f;
 
 //Not finished, just a start to the file.
 public class PhysicsEngine {
+    
+    public class PhysicsConstants {
+        public static final float G_FORCE = -9.81f;
+        public static final float AIR_RESISTANCE = 0f; //temp
+        public static final float TIME_STEP = 1f/60f;
+
+    }
+
     Vector3f vector; //temp
 
 }
